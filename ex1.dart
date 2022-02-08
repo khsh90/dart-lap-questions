8e981d565fc2b09c138dc0fc2f2d50a3d8ebb704
Khaled Hadidi <khsh400@gmail.com>
2022-02-08 15:08:44 +0200

    finish exrecis 3

---
+++ ex1.dart
@@ -0,0 +1,14 @@
+// Ex4 :and write a program that prints out all the elements of the list that are less than 5.
+
+// void main() {
+//   var a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+//   for (var number in a) {
+//     if (number >= 5) {
+//       print(number);
+//     }
+//   }
+// }
+// ----------------------------------------------------------
+// Create a program that asks the user for a
+// number and then prints out a list of all the divisors of that number.
