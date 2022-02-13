1011aff92e062396a3cb0da9fa0e59b05779caea
Khaled Hadidi <khsh400@gmail.com>
2022-02-13 15:49:21 +0200

    ex8

--- ex1.dart
+++ ex1.dart
@@ -1,14 +1,3 @@
-// Ex4 :and write a program that prints out all the elements of the list that are less than 5.
-
-// void main() {
-//   var a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-
-//   for (var number in a) {
-//     if (number >= 5) {
-//       print(number);
-//     }
-//   }
-// }
 // -------------------------------------------------------------
 // ex1 :Create a program that asks the user to enter their name and their age
 //. Print out a message that tells how many years they have to be 100 years old.
@@ -59,7 +48,40 @@
 //   }
 // }
 // --------------------------------------------------------------------------
+// // Ex3 :and write a program that prints out all the elements of the list that are less than 5.
 
+// void main() {
+//   var a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+//   for (var number in a) {
+//     if (number >= 5) {
+//       print(number);
+//     }
+//   }
+// }
+// -----------------------------------------------------------------------------
+// Ex4:
+// import 'dart:io';
+
+// void main() {
+//   while (true) {
+//     stdout.write('Enter a number to check if it is divisor:');
+//     final userInput = stdin.readLineSync();
+
+//     if (userInput != null && userInput.isNotEmpty) {
+//       final numb = double.tryParse(userInput);
+
+//       if (numb != null) {
+//         for (var i = 1; i <= numb; i++) {
+//           if (numb % i == 0) {
+//             print(i);
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+// ----------------------------------------------------------------------
 //ُEx5: Take two lists, for example:
 // a [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 // b [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
@@ -77,7 +99,7 @@
 //   print(c);
 // }
 
-//another way . 
+//another way .
 // void main() {
 //   List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
 //   List<int> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 89];
@@ -96,4 +118,90 @@
 //   print(Set.from(a).intersection(Set.from(b)).toList());
 // }
 // ----------------------------------------------------
+// ex6:Ask the user for a string and print out whether this string is a palindrome or not.
+// A palindrome is a string that reads the same forwards and backwards.
+// radar
 
+// ask user=>stdout.write();
+//user word inpuut=>stdin.readLineSync()
+
+// import 'dart:io';
+
+// void main() {
+//   while (true) {
+//     stdout.write('Please enter a word to check if it is palindrome:');
+//     final userInput = stdin.readLineSync();
+
+//     if (userInput != null && userInput.isNotEmpty) {
+//       final resverseInput = userInput.split('').reversed.join();
+
+//       if (userInput == resverseInput) {
+//         print('The word $userInput is palindrome');
+//       } else {
+//         print('The word $userInput is not palindrome');
+//       }
+//     }
+//   }
+// }
+// --------------------------------------------------------------------------
+// ex 7 :
+// Let’s say you are given a list saved in a variable:
+// a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100].
+// Write a Dart code that takes this list and makes a new list that has only the even elements of this list in it.
+
+// void main() {
+//   final a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+//   List evenNumbers = [];
+//   for (var number in a) {
+//     if (number % 2 == 0) {
+//       evenNumbers.add(number);
+//     }
+//   }
+//   print(evenNumbers);
+// }
+// --------------------------------------------------------------------
+// EX8:
+// Make a two-player Rock-Paper-Scissors game against computer.
+// Ask for player’s input, compare them, print out a message to the winner.
+// Start---
+
+import 'dart:io';
+
+import 'dart:math';
+
+var rock = 'Rock';
+var paper = 'Paper';
+var scissors = 'Scissors';
+
+enum Options { rock, paper, scissors }
+void main() {
+  while (true) {
+    stdout.write('Please select (R)ock,(P)aper or (S)cissors:');
+    final userInput = stdin.readLineSync();
+
+    // -----------------------------------------------------------------
+    final index = Random().nextInt(Options.values.length);
+    final computerSelection = Options.values[index].name;
+
+    // -----------------------------------------------------------------
+
+    if (userInput != null && userInput.isNotEmpty) {
+      if (userInput == Options.) {
+        print('you select ${Options.rock.name}');
+        print(computerSelection);
+      } else if (userInput == 'P' ||
+          userInput == 'Paper' ||
+          userInput == 'p' ||
+          userInput == 'paper') {
+        print('you select ${Options.paper.name}');
+      } else if (userInput == 'S' ||
+          userInput == 'Scissors' ||
+          userInput == 's' ||
+          userInput == 'scissors') {
+        print('you select ${Options.scissors.name}');
+      } else {
+        print('Invalid selection');
+      }
+    }
+  }
+}
