6d56020273391cc5a378b07d1836f24940c30484
Khaled Hadidi <khsh400@gmail.com>
2022-02-23 16:35:47 +0200

    add ex25

--- ex24.dart
+++ ex24.dart
@@ -70,3 +70,28 @@ void word({required String correctWord}) {
     }
   }
 }
+
+
+// ----------------------------------------------------------
+/*
+Exercise 25
+In this exercise, we will finish building Hangman. In the game of Hangman,
+ the player only has 6 incorrect guesses (head, body, 2 legs, and 2 arms) before they lose the game.
+
+In Part 1, we loaded a random word list and picked a word from it.
+In Part 2, we wrote a logic for guessing the letter and displaying that information to user.
+In this exercise, we have to put it all together and add logic for handling guesses.
+
+Copy your code from Parts 1 and 2 into a new file as a starting point. Now add the following features:
+
+Only let the user guess 6 times, and tell the user how many guesses they have left.
+Keep track of the letters user guessed. If the user guesses a letter they had already 
+guessed, don’t penalize them - let them guess again.
+Optional additions:
+
+When the player wins or loses, let them start a new game.
+Rather than telling the user “You have 4 incorrect guesses left”, display some
+ picture art for the Hangman. This is challenging - do the other parts of the exercise first!
+Your solution will be a lot cleaner if you make use of functions to help you!
+
+*/
